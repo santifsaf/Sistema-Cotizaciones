ba95440ad25194632fb5fefe3577487cbaf55254
Santiago Fuentes <142640446+santifsaf@users.noreply.github.com>
2024-06-15 17:40:25 -0300

    Crea los models

--- facturacionApp/admin.py
+++ facturacionApp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Empresa, Clientes, Articulos, Cotizaciones
 
 # Register your models here.
+
+class EmpresaAdmin(admin.ModelAdmin):
+    readonly_fields=('created', 'updated')
+
+class ClientesAdmin(admin.ModelAdmin):
+    readonly_fields=('created', 'updated')
+
+class ArticulosAdmin(admin.ModelAdmin):
+    readonly_fields=('created', 'updated')
+
+class CotizacionesAdmin(admin.ModelAdmin):
+    readonly_fields=('created', 'updated')
+
+admin.site.register(Empresa, EmpresaAdmin)
+admin.site.register(Clientes, ClientesAdmin)   
+admin.site.register(Articulos, ArticulosAdmin)
+admin.site.register(Cotizaciones, CotizacionesAdmin)   
