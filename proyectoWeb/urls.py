5dcb9d0fd7c55c060c752ade05f6b89e5dddaf96
Santiago <santiagoafuentes@gmail.com>
2025-08-06 23:04:20 -0300

    correccion urls principales

--- proyectoWeb/urls.py
+++ proyectoWeb/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from cotizApp.views import home
+from cotizApp.views import Home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,7 +11,7 @@ urlpatterns = [
     path('clientes/', include('clientes.urls')),
     path('cotizaciones/', include('cotizaciones.urls')),
     path('accounts/', include('login.urls')), 
-    path('', home, name='home'),  
+    path('', Home, name='home'),  
 ]
 
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
