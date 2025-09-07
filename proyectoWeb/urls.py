7547a129fc183c9029575b254565d2300d9a6bf1
Santiago <santiagoafuentes@gmail.com>
2025-09-07 15:45:52 -0300

    Migracion bdd

--- proyectoWeb/urls.py
+++ proyectoWeb/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
-from cotizApp.views import Home
 from django.conf import settings
+from cotizApp.views import Home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
